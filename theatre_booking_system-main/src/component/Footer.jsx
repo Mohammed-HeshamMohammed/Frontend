6cb43486a2d6ce7f3c733c5db42ccab6d75bc36a
Mohammed-HeshamMohammed <144591912+Mohammed-HeshamMohammed@users.noreply.github.com>
2023-10-09 18:47:39 +0300

    Add files via upload

--- theatre_booking_system-main/src/component/Footer.jsx
+++ theatre_booking_system-main/src/component/Footer.jsx
@@ -3,39 +3,40 @@ import { NavLink } from "react-router-dom";
 import logo from "../images/logo.jpg";
 import { faFacebook, faGoogle, faInstagram } from "@fortawesome/fontawesome-free-brands";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '../css/Footer.css';
 
 function Footer() {
   return (
-    <div className="footer bg-black flex-md-row gap-4">
-      <div className="imageFooter">
+    <footer className="navbar-expand-lg d-flex justify-content-around text-dark">
+      <div className="logo">
         <img src={logo} alt="logo" />
       </div>
-      <div className="list text-white">
-        <h2>Servises</h2>
-        <ul>
-        <li className="nav-item">
-            <NavLink to="/home/food" className="nav-link text-white">
+      <div className="" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto text-dark flex-sm-row flex-md-row">
+          <li className="nav-item text-white">
+            <NavLink to="/home/food" className="nav-link">
               Foods' Court
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink to="/home/drink" className="nav-link text-white">
+          <li className="nav-item text-white">
+            <NavLink to="/home/drink" className="nav-link">
               Drinks' Court
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink to="/home/reserveSeats" className="nav-link text-white">
+          <li className="nav-item text-white">
+            <NavLink to="/home/reserveSeats" className="nav-link">
               Movies
             </NavLink>
           </li>
         </ul>
       </div>
+
       <div className="icons text-white">
         <FontAwesomeIcon icon={faFacebook} />
         <FontAwesomeIcon icon={faGoogle} />
         <FontAwesomeIcon icon={faInstagram} />
       </div>
-    </div>
+    </footer>
   );
 }
 
--- theatre_booking_system-main/src/component/Navs.jsx
+++ theatre_booking_system-main/src/component/Navs.jsx
@@ -1,101 +1,102 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "../css/Navs.css";
 import logo from "../images/logo.jpg";
-import { FaShoppingCart, FaRegMoon, FaSun} from "react-icons/fa";
-
+import { FaShoppingCart, FaRegMoon, FaSun } from "react-icons/fa";
+import SearchBar from "./SearchBar";
 function Navs() {
   const [nightMode, setNightMode] = useState(false);
-const toggleNightMode = () => {
-  setNightMode(!nightMode);
 
-  // Additional logic for toggling styles based on night mode
-  const body = document.body;
-  const navLinks = document.querySelectorAll(".nav-link");
-  const logoImage = document.querySelector(".logo img");
-  const footer = document.querySelector(".footer");
-
-  if (nightMode) {
-    // Night mode styles
-    body.style.backgroundColor = "#000";
-    navLinks.forEach(link => {
-      link.style.color = "#fff";
-    });
-    logoImage.style.filter = "invert(1)";
-    footer.style.backgroundColor = "#000";
-    footer.style.color = "#fff";
-  } else {
-    // Day mode styles
-    body.style.backgroundColor = "#fff";
-    navLinks.forEach(link => {
-      link.style.color = "#000";
-    });
-    logoImage.style.filter = "invert(0)";
-    footer.style.backgroundColor = "#fff";
-    footer.style.color = "#000";
+  useEffect(() => {
+    const body = document.body;
+    const navLinks = document.querySelectorAll(".nav-link");
+    const logoImage = document.querySelector(".logo img");
+    const footer = document.querySelector(".footer");
+  
+    if (nightMode) {
+      // Night mode styles
+      body.style.backgroundColor = "#000";
+      navLinks.forEach(link => {
+        link.style.color = "#fff";
+      });
+      if (logoImage) logoImage.style.filter = "invert(1)";
+      if (footer) {
+        footer.style.backgroundColor = "#000";
+        footer.style.color = "#fff";
+      }
+    } else {
+      // Day mode styles
+      body.style.backgroundColor = "#fff";
+      navLinks.forEach(link => {
+        link.style.color = "#000";
+      });
+      if (logoImage) logoImage.style.filter = "invert(0)";
+      if (footer) {
+        footer.style.backgroundColor = "#fff";
+        footer.style.color = "#000";
+      }
+    }
+  }, [nightMode]);
+  
+  function toggleNightMode() {
+    setNightMode(!nightMode);
   }
-};
 
+
+
+
+
+  
   return (
-    <nav className="navbar-expand-lg d-flex justify-content-around  text-dark">
+    <nav className="navbar-expand-lg d-flex justify-content-between align-items-center text-dark">
       <div className="logo">
         <img src={logo} alt="logo" />
-
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
       </div>
+      <SearchBar/>
 
-      <div className="" id="navbarSupportedContent">
+      <div className="nav-links" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto text-dark flex-sm-row flex-md-row">
           <li>
-              <div className="night-mode-toggle" onClick={toggleNightMode}>
-            {nightMode ? (
-              <FaSun size={30} className="night-mode-icon text-white" />
-            ) : (
-              <FaRegMoon size={30} className="night-mode-icon text-dark" />
-            )}
-          </div>
+            <div className="night-mode-toggle" onClick={toggleNightMode}>
+              {nightMode ? (
+                <FaSun size={30} className="night-mode-icon text-white" />
+              ) : (
+                <FaRegMoon size={30} className="night-mode-icon text-dark" />
+              )}
+            </div>
           </li>
           <li className="nav-item active">
-            <NavLink className="nav-link " to="/home" exact>
+            <NavLink className="nav-link" to="/home" exact>
               Home
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/home/food" className="nav-link ">
+            <NavLink to="/home/food" className="nav-link">
               Foods' Court
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/home/drink" className="nav-link ">
+            <NavLink to="/home/drink" className="nav-link">
               Drinks' Court
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink to="/home/reserveSeats" className="nav-link ">
+            <NavLink to="/home/reserveSeats" className="nav-link">
               Movies
             </NavLink>
           </li>
           <li>
-          <div className="cart-icon">
-        <NavLink to="/home/cart" className="nav-link ">
-          <FaShoppingCart size={30} />
-        </NavLink>
-      </div>
+            <div className="cart-icon">
+              <NavLink to="/home/cart" className="nav-link">
+                <FaShoppingCart size={30} />
+              </NavLink>
+            </div>
           </li>
         </ul>
       </div>
+  
       <div className="form">
-        <form class="form-inline my-2 my-lg-0">
+        <form className="form-inline my-2 my-lg-0">
           <button className="btn btn-danger mx-3">
             <NavLink className="text-white" to="/">
               Logout
@@ -105,6 +106,7 @@ const toggleNightMode = () => {
       </div>
     </nav>
   );
+  
 }
 
 export default Navs;
